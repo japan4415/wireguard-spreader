63e41ad530bb153c8e2065405d70e9a603e55c9e
Yoshihiko Kato <katoyoshihiko@lifull.com>
2022-11-28 10:30:57 +0900

    a

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,50 @@ enum Modes {
     },
 }
 
-fn main() {
+
+use tonic::{transport::Server, Request, Response, Status};
+use server::greeter_server::{Greeter, GreeterServer};
+use server::{HelloRequest, HelloReply};
+
+pub mod server {
+    tonic::include_proto!("server");
+}
+
+#[derive(Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let reply = server::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
     match &cli.mode {
         Some(Modes::Server { port }) => {
-            println!("Server start on {:?}",port);
+            let addr = "[::1]:50051".parse().unwrap();
+            let greeter = MyGreeter::default();
+        
+            println!("GreeterServer listening on {}", addr);
+        
+            Server::builder()
+                .add_service(GreeterServer::new(greeter))
+                .serve(addr)
+                .await?;
+        
+            Ok(())
         }
-        None => {}
+        None => {Ok(())}
     }
 }
